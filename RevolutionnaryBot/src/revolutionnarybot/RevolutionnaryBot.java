6660548cb0cbcc782abc9bd8d264fd43f2d77766
Jereem <jereeem18@gmail.com>
2014-01-14 09:08:00 +0100

    ajout MaliciousRobot.java

--- RevolutionnaryBot/src/revolutionnarybot/RevolutionnaryBot.java
+++ RevolutionnaryBot/src/revolutionnarybot/RevolutionnaryBot.java
@@ -4,6 +4,8 @@
  */
 package revolutionnarybot;
 
+import java.awt.AWTException;
+
 /**
  *
  * @author jeremygillet
@@ -13,7 +15,10 @@ public class RevolutionnaryBot {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws AWTException {
         // TODO code application logic here
+       MaliciousRobot bot = new MaliciousRobot(100,10);
+       
+        
     }
 }
